3cd4c7e5830bcd3e17da2ed9510e145a9750e145
Gabriel Cesar Gravena <spfcgabreu@gmail.com>
2025-05-21 15:17:30 -0300

    feat: aplicar ResponseEntity em endpoints GET e DELETE

--- src/main/java/com/example/crud_produto/controller/ProdutoController.java
+++ src/main/java/com/example/crud_produto/controller/ProdutoController.java
@@ -3,9 +3,8 @@ package com.example.crud_produto.controller;
 
 import com.example.crud_produto.model.Produto;
 import com.example.crud_produto.services.ProdutoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,23 @@ public class ProdutoController {
     @GetMapping()
     public List<Produto> findAll() {
         return produtoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id) {
+        return produtoService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Produto create(Produto produto) {
+        return produtoService.saveProduto(produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        produtoService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
